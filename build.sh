ccaa01b864a6657d7df74af437b2d712ca9cac9f
jb747 <108913053+jb747@users.noreply.github.com>
2024-10-03 17:02:23 -0400

    wip

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -m
 
 trap 'ret=$?; echo "$0:$LINENO: Error: set -e triggered"; exit $ret' ERR
 
-declare -a actions=("release_test_delve_image")
+declare -a actions=("release_test_hydra_image")
 for a in "${actions[@]}"; do
   gh workflow run "${a}.yml" --ref main || true
 done
